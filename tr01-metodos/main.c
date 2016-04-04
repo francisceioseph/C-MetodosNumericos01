284b5443fbc945a30cc337768ad53e6af9280d7c
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2016-04-04 00:41:52 -0300

    ler arquivo

--- tr01-metodos/main.c
+++ tr01-metodos/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 #define EPSILON 0.00000001
@@ -8,13 +9,59 @@ double** alocaMatriz(int l, int c);
 void imprimeMatriz(double **m, int l, int c);
 void desalocaMatriz(double  **m, int l);
 
+int linha;
+
 int main()
 {
-    double** c = alocaMatriz(5, 5);
+    char nome[50];
+    printf("Nome do arquivo = ");
+    scanf("%s",nome);
 
-    desalocaMatriz(c, 5);
+    double** c = alocaMatriz(3, 4);
 
+    lerMatrizDoArquivo(c, nome);
+
+    desalocaMatriz(c, 3);
+
+    system("pause");
     return 0;
+}
+
+void lerMatrizDoArquivo(double **m, char *filename[]){
+     /*lê valores de um arquivo para uma matriz de inteiros com l linhas e c colunas*/
+    //char url = filename[1];
+    char ch;
+
+    FILE *arq = fopen(filename,"r");
+    int i=-1;
+    int j=-1;
+
+    if(arq==NULL){
+        printf("Erro ao abrir arquivo!\n");
+    }else{
+
+        ch=fgetc(arq);
+        linha = ch-48; //converte o caractere ch para inteiro
+
+        printf("Ordem da matriz = %d\n",linha);
+
+        while( (ch=fgetc(arq))!= EOF ){
+
+            if(ch!=' ' && ch!='\n'){
+
+                m[2][1]=2;  //adicionar elementos a matriz
+                j++;
+                printf("elemento = %d\n",ch-48);
+            }else if(ch=='\n'){
+
+                printf("muda de linha = %d\n",i);
+                i++;
+                j=0;
+            }
+        }
+    }
+
+    fclose(arq);
 }
 
 // Funcoes genericos de manipulação de matrizes
@@ -43,11 +90,11 @@ double** alocaMatriz(int l, int c) {
     int i, j;
     double **m;
 
-    m = malloc(sizeof(double*) * l);
+    m = malloc(sizeof(double*)*l);
 
     if (m != NULL) {
         for (i = 0; i < l; i++) {
-            m[i] = malloc(sizeof(double) * c);
+            m[i] = malloc(sizeof(double*)*c);
 
             if (m[i] == NULL) {
 
@@ -83,8 +130,3 @@ void desalocaMatriz(double **m, int l)
     }
 }
 
-// Funcoes para implementar o metodo da pivotacao completa.
-
-void lerMatrizDoArquivo(char *filename) {
-
-}
